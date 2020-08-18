ff91754a5379c19ac1435a5b7706bc642be3ef09
robertsja <63753397+robertsja@users.noreply.github.com>
2020-08-18 15:10:37 +1200

    attack and users and enemy created, half of turns

--- dental_RPG.py
+++ dental_RPG.py
@@ -6,3 +6,31 @@
 # simulator that will be designed to teach
 # young people about dental hygiene. 
 
+
+def characters():
+    """ Gives user a character name """
+    character = input('What is your name: ')
+    enemy = 'plaque'
+    return character, enemy
+
+
+def player_attack():
+    attack_verify = input("Press Enter to attack ").strip()
+    return True 
+
+
+
+def turns():
+    for i in range(0, 100):
+        attack = player_attack()
+        if attack == True:
+            print("You have attacked")
+        
+
+        
+        
+    
+    
+    
+    
+
