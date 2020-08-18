4a2033d34bf1bbbbad4d7c9e3a5c9685153842f6
robertsja <63753397+robertsja@users.noreply.github.com>
2020-08-18 14:32:16 +1200

    Initialization of program

---
+++ dental_RPG.py
@@ -0,0 +1,8 @@
+##
+# dental_RPG.py
+# By: Jarrod Roberts
+# Created on: 18/08/20
+# Create an You are going to develop an RPG
+# simulator that will be designed to teach
+# young people about dental hygiene. 
+
