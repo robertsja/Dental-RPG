0f8090cba466bf9a65f4f4cc82d8188d1eee7a9f
robertsja <63753397+robertsja@users.noreply.github.com>
2020-08-24 10:42:21 +1200

    Created Rock Paper Scissors type attacks

--- dental_RPG.py
+++ dental_RPG.py
@@ -13,66 +13,101 @@ import random
 def characters():
     """ Gives user a character name """
     character = input('What is your name: ')
-    enemy = 'Plaque'
-    return character, enemy
+    return character
 
 
-def player_attack():
+def user_attack():
     """ Asks user to select type of attack """
     end = False
     attack_types = ['1', '2', '3']
     while end == False:
-        attack_u = ''
+        attack_u_num = ''
         while not (attack_u_num in attack_types):
             attack_u_num = str(input('''What type of attack do you want to do:
-1: Rock
-2: Paper
-3: Scissors
+1: Tooth Brush (Beats Plaque, loses to Bad Breath, Draws against Gum Disease)
+2: Mouth Wash (Beats Bad Breath, Loses to Gum disease, Draws Plaque)
+3: Dental Floss (Beats Gum Disease, Loses to Plaque, Draws against Bad Breath)
 
-: '''))
+: ''')).strip()
 
-    if attack_u_num == '1':
-        attack_u = 'Rock'
-        end = True
-        
-    elif attack_u_num == '2':
-        attack_u = 'Paper'
-        end = True
-        
-    elif attack_u_num == '3':
-        attack_u = 'Scissors'
-        end = True
-        
-    else:
-        end = False
+        if attack_u_num == '1':
+            attack_u = 'Tooth Brush'
+            end = True
+            
+        elif attack_u_num == '2':
+            attack_u = 'Mouth Wash'
+            end = True
+            
+        elif attack_u_num == '3':
+            attack_u = 'Dental Floss'
+            end = True
+            
+        else:
+            end = False
     print('You have picked {}'.format(attack_u))
+
     return attack_u
 
 
 def enemy_attack():
     """ Enemy randomly selects between R P or S """
     attack_e_num = random.randint(1, 3)
-    print(attack_e_num)
+    if attack_e_num == 1:
+        attack_e = 'Plaque'
+            
+    elif attack_e_num == 2:
+        attack_e = 'Bad Breath'
+            
+    elif attack_e_num == 3:
+        attack_e = 'Gum Disease'
+
+    return attack_e
     
 
 
-def turns(e_name):
-    for i in range(0, 100):
-        attack_p = player_attack()
-        if attack_p == True:
-            print("You have attacked\n")
-        else:
-            continue
+def turns():
+    win = 0
+    loss = 0
+    for i in range(0, 5):
+        print()
+        attack_u = user_attack()
         attack_e = enemy_attack()
-        if attack_e == True:
-            print('{} has attacked\n'.format(e_name))
+        if ((attack_u == 'Tooth Brush' and attack_e == 'Plaque')
+        or (attack_u == 'Mouth Wash' and attack_e == 'Bad Breath')
+        or (attack_u == 'Dental Floss' and attack_e == 'Gum Disease')):
+            print('\nYou used {} and beat {}. Good Job!'.format(attack_u, attack_e))
+            win += 1
+
+        elif (attack_u == 'Tooth Brush' and attack_e == 'Bad Breath'):
+            print('\nUh Oh. You have got {}. You need to use Mouth Wash.'.format(attack_e))
+            loss += 1
+            
+        elif (attack_u == 'Mouth Wash' and attack_e == 'Gum Disease'):
+            print('\nUh Oh. You have got {}. You need to use Dental Floss.'.format(attack_e))
+            loss += 1
+            
+        elif (attack_u == 'Dental Floss' and attack_e == 'Plaque'):
+            print('\nUh Oh. You have got {}. You need to use Tooth Brush.'.format(attack_e))
+            loss += 1
         else:
-            continue
+            print('\n{} doesn\'t effect {}.'.format(attack_u, attack_e))
+            
+    return win, loss
+    
+
+def win_loss(win, loss, username):
+    """ Totals wins and loss and tells if the user won """
+    if win > loss:
+        print('Good Job {}, you won {} out of 5 rounds and won!'.format(username, win))
+    elif win == loss:
+        print('{}, you drew with bad dental hygiene, both winning {} rounds each'.format(username, win))
+    else:
+        print('{}, you have lost to bad dental hygiene. You only beat it {} times'.format(username, win))
 
 def main():
-    username, e_name = characters()
-    turns(e_name)
-    
+    username = characters()
+    win, loss = turns()
+    win_loss(win, loss, username)
     
 
         
