87f86f5fb1d7cf0934d8c353cf6118c32721188f
robertsja <63753397+robertsja@users.noreply.github.com>
2020-09-08 15:08:29 +1200

    Finished 4 moves attacking and HP out of 100

--- dental_RPG.py
+++ dental_RPG.py
@@ -18,6 +18,40 @@ def characters():
     print('Welcome {}\n'.format(character))
     return character
 
+def hp(username, user_hp, enemy, enemy_hp, user_damage, enemy_damage):
+    """ Changes users and CPU's hit points"""
+    user_hp -= enemy_damage
+    enemy_hp -= user_damage
+    # If hp goes below 0, it won't show negative numbers
+    if user_hp < 0:
+        print('''-------------------------------
+{} HP: 0%
+{} HP: {}%
+-------------------------------
+You lost to bad dental hygiene :(
+          '''.format(username, enemy, enemy_hp))
+    elif enemy_hp < 0:
+        print('''-------------------------------
+{} HP: {}%
+{} HP: 0%
+-------------------------------
+You beat bad dental hygiene!
+          '''.format(username, user_hp, enemy))
+    elif user_hp < 0 and enemy_hp < 0:
+        print('''-------------------------------
+{} HP: 0%
+{} HP: 0%
+-------------------------------
+You drew to bad dental hygiene :|
+          '''.format(username, enemy))
+    else:
+        print('''-------------------------------
+{} HP: {}%
+{} HP: {}%
+-------------------------------
+              '''.format(username, user_hp, enemy, enemy_hp))
+    return user_hp, enemy_hp
+
 
 def weapon_pick():
     """ User picks weapon """
@@ -70,56 +104,74 @@ def weapon_pick():
 
 def moves_type(weapon):
     """ Assigns the moves to dict and list for later use """
-    if weapon == 'Tooth Brush':
-        moves_dict = {'30 Sec Brush':10, 'Hard Bristle':15,
-                 'Circular Motion':20, 'Perfect Brush':25}
 
-        moves_list = '30 Sec Brush', 'Hard Bristle',
-        'Circular Motion', 'Perfect Brush']
+    if weapon == 'Tooth Brush':
+        moves_dict = {10:'30 Sec Brush', 15:'Hard Bristle',
+                      20:'Circular Motion', 25:'Perfect Brush'}
         
     elif weapon == 'Mouth Wash':
-        moves_dict = {'Swish Swish':10, 'Gargle':15,
-                 'Gargle Rinse':20, 'Perfect Wash':25}
+        moves_dict = {10:'Swish Swish', 15:'Gargle',
+                      20:'Gargle Rinse', 25:'Perfect Wash'}
 
-        moves_list[]
     else:
-        moves_dict = {'Bad Technique':10, 'Cutting Gums':15,
-                 'Thorough Floss':20, 'Perfect Floss':25}
+        moves_dict = {10:'Bad Technique', 15:'Cutting Gums',
+                 20:'Thorough Floss', 25:'Perfect Floss'}
+
     return moves_dict
         
 
-def user_attack():
+def user_attack(weapon, moves, username):
     """ Asks user to select type of attack """
-    end = False
-    attack_types = ['1', '2', '3', '4']
-    while end == False:
-        attack_u_num = ''
-        while not (attack_u_num in attack_types):
-            attack_u_num = str(input('''What type of attack do you want to do:
-1: Tooth Brush (Beats Plaque, loses to Bad Breath, Draws against Gum Disease)
-2: Mouth Wash (Beats Bad Breath, Loses to Gum disease, Draws Plaque)
-3: Dental Floss (Beats Gum Disease, Loses to Plaque, Draws against Bad Breath)
 
-: ''')).strip()
+    count = 0
+    print('-------------------------------')
+    print('Attacks:')
+    for damage, move in sorted(moves.items()):
+        count += 1
+        print('{}: {}: {}DMG'.format(count, move, damage))
+    valid_input = ['1', '2', '3', '4']
+    attack = ''
+    while not(attack in valid_input):
+        attack = input('\nPick your attack: ')
+        print('')
 
-        if attack_u_num == '1':
-            attack_u = 'Tooth Brush'
-            end = True
-            
-        elif attack_u_num == '2':
-            attack_u = 'Mouth Wash'
-            end = True
-            
-        elif attack_u_num == '3':
-            attack_u = 'Dental Floss'
-            end = True
-            
-        else:
-            end = False
-    print('You have picked {}'.format(attack_u))
+    for damage, move in moves.items():
+        if (attack == '1') and (damage == 10):
+            print('{} has used {} dealing {} DMG'.format(username, move, damage))
+            return damage
+        
+        elif (attack == '2') and (damage == 15):
+            print('{} has used {} dealing {} DMG'.format(username, move, damage))
+            return damage
+        
+        elif (attack == '3') and (damage == 20):
+            print('{} has used {} dealing {} DMG'.format(username, move, damage))
+            return damage
+        
+        elif (attack == '4') and (damage == 25):
+            print('{} has used {} dealing {} DMG'.format(username, move, damage))
+            return damage
 
-    return attack_u
 
+def enemy_attack():
+    """ Randomly picks an attack for the enemy to pick """
+    num = random.randint(1, 4)
+    if num == 1:
+        damage = 10
+        attack = 'Plaque'
+    elif num == 2:
+        damage = 15
+        attack = 'Chipped Tooth'
+    elif num == 3:
+        damage = 20
+        attack = 'Cavity'
+    else:
+        damage = 25
+        attack = 'Oral Cancer'
+        
+    return damage, attack
+    
+    
 
 def enemy_select():
     """ Enemy randomly selected """
@@ -138,37 +190,23 @@ def enemy_select():
 
 
 def turns(weapon, moves, username):
-
-    count = 0
-    enemy = enemy_select()
+    """ Swaps between user attacking and CPU attacking """
+    # Preset values
     enemy_hp = 100
     user_hp = 100
-    print('''-------------------------------
-{} HP: {}
-{} HP: {}
--------------------------------
-          '''.format(username, user_hp, enemy, enemy_hp))
-    print('Attacks:')
-    for move, damage in moves.items():
-        count += 1
-        print('{}: {}: {}DMG'.format(count, move, damage))
-    valid_input = ['1', '2', '3', '4']
-    attack = ''
-    while not(attack in valid_input):
-        attack = input('Pick your attack: ')
+    user_damage = 0
+    enemy_damage = 0
+    enemy = enemy_select()
+    enemy_hp, user_hp = hp(username, user_hp, enemy, enemy_hp, user_damage, enemy_damage)
     
-
-    
-    
-
-def win_loss(win, loss, username):
-    """ Totals wins and loss and tells if the user won """
-    if win > loss:
-        print('Good Job {}, you won {} out of 5 rounds and beat Bad Dental Hygiene!'.format(username, win))
-    elif win == loss:
-        print('{}, you drew with bad dental hygiene, both winning {} rounds each'.format(username, win))
-    else:
-        print('{}, you have lost to bad dental hygiene. You only beat it {} times'.format(username, win))
+    # Calls functions while both characters HP is above 0
+    while (enemy_hp > 0) and (user_hp > 0):
+        user_damage = user_attack(weapon, moves, username)
+        enemy_damage, enemy_move = enemy_attack()
+        print('{} used {} dealing {} DMG'.format(enemy, enemy_move, enemy_damage))
+        print('-------------------------------')
+        user_hp, enemy_hp = hp(username, user_hp, enemy, enemy_hp, user_damage, enemy_damage)
+        
 
 def main():
     username = characters()
